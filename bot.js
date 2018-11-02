f9421fb7d9b8b550289bd2614e336c24c027bdee
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-02 22:54:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-var ServerID = "507694894295416832";// اي دي الروم
-var ChannelID = "507696158983061505";
+var ServerID = "505093047369990145";// اي دي الروم
+var ChannelID = "507645194942808085";
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
